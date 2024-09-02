88896dd933353270989e17be8d6dbfc47581074f
adity365 <aditya.agarwal.20.08.2002@gmail.com>
2024-09-02 07:04:57 +0530

    Create exp_11(a).cpp

---
+++ exp_11(a).cpp
@@ -0,0 +1,5 @@
+# Experiment-7: To study the basics of Object Oriented Programming 
+
+## Aim
+To learn how to implement Object Oriented Concepts like defining class, object, defining Function inside class,using public and private access modifiers, display function inside class 
+## Theory
